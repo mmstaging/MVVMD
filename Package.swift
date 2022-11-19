96a473fe398260898d4ffb7e9ceab237be93d093
mm <mmdevstuff@gmail.com>
2022-11-18 17:16:57 -0800

    update

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "mmpub.MVVM",
+    name: "MVVM",
     platforms: [
         .macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)
     ],
