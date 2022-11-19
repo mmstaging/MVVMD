1727709ad7c4204d30c97c73e393e4fa267bb419
mm <mmdevstuff@gmail.com>
2022-11-18 18:06:14 -0800

    update

--- Sources/MVVM/Model/DataManager.swift
+++ Sources/MVVM/Model/DataManager.swift
@@ -4,16 +4,16 @@ import Foundation
 
 public class MVVMDataManager: SingleInstance {
     // responsibilities: create DAO, manage datasources.
-    private var dataSources = [DataSource]()
+    private var dataSources = [MVVMDataSource]()
 
     public required init?() {
     }
 
-    public func add(dataSource: DataSource) {
+    public func add(dataSource: MVVMDataSource) {
         dataSources.append(dataSource)
     }
 
-    public func createDataAccessObject(id: String) -> DataAccessObject? {
+    public func createDataAccessObject(id: String) -> MVVMDataAccessObject? {
         let components = id.split(separator: ".")
         guard components.count == 2 else { fatalError("createDataAccessObject requires two components, got \"\(id)\"") }
         guard let dataSource = dataSources.first(where: {components[0] == $0.dataSourceID}) else { return nil }
