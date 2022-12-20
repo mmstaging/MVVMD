86d1b3d5e65a4b803a7e3e0532d4f3394c815ad0
mm <mmdevstuff@gmail.com>
2022-12-20 11:07:15 -0800

    update

--- Sources/MVVMD/Core/DataSource.swift
+++ Sources/MVVMD/Core/DataSource.swift
@@ -4,7 +4,7 @@ import Foundation
 import SingleInstance
 
 public protocol MVVMD_DataSource: SingleInstance {
-    var state: DataSourceState
+    var state: DataSourceState { get }
     var dataSourceID : String { get }
     var params: [String:String] { get }
     func createDataAccessObject(id: String, params:[String:String]) -> MVVMD_DataAccessObject?
